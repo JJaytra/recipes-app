8f3e2cf74adb2f493fd8f98bc29f4df101171517
Jamie <jaytra23@gmail.com>
2024-04-05 11:48:54 +1100

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import Navbar from "@/components/navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ChakraProvider>
+    
+        <body className={inter.className}>{children}</body>
+      </ChakraProvider>
     </html>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <span className='title'>Nav</span>
+        </div>
+    )
+}
+
+export default Navbar
